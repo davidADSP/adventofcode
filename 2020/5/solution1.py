54f7ab4a34b1aca1dad4a3ffb5c565150e1d34a4
David Foster <david@applied-data.science>
2020-12-05 09:55:34 +0000

    update day 5

--- 2020/5/solution1.py
+++ 2020/5/solution1.py
@@ -1,8 +1,4 @@
 #script
 data = open("input.txt", "r").read().splitlines()
-seats = []
-for x in data:
-    seat = int('0b' + ''.join(['0' if l in ('F', 'L') else '1' for l in x]), 2)
-    seats.append(seat)
-
+seats = [int('0b' + ''.join(['0' if l in ('F', 'L') else '1' for l in x]), 2) for x in data]
 print(max(seats))
--- 2020/5/solution2.py
+++ 2020/5/solution2.py
@@ -1,11 +1,8 @@
 #script
 data = open("input.txt", "r").read().splitlines()
-seats = []
-for x in data:
-    seat = int('0b' + ''.join(['0' if l in ('F', 'L') else '1' for l in x]), 2)
-    seats.append(seat)
-
+seats = [int('0b' + ''.join(['0' if l in ('F', 'L') else '1' for l in x]), 2) for x in data]
 seats.sort()
+
 current_seat = seats[0]
 for seat in seats:
     if seat != current_seat:
