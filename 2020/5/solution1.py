257fd588b042df2e3600ece9ed4cfd413352eda9
David Foster <david@applied-data.science>
2020-12-05 09:54:25 +0000

    update day 5

--- 2020/5/solution1.py
+++ 2020/5/solution1.py
@@ -2,9 +2,7 @@
 data = open("input.txt", "r").read().splitlines()
 seats = []
 for x in data:
-    row = int('0b' + ''.join(['0' if l == 'F' else '1' for l in x[:7]]), 2)
-    col = int('0b' + ''.join(['0' if l == 'L' else '1' for l in x[7:]]), 2)
-
-    seats.append(8 * row + col)
+    seat = int('0b' + ''.join(['0' if l in ('F', 'L') else '1' for l in x]), 2)
+    seats.append(seat)
 
 print(max(seats))
