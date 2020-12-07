64675ed993ffa09ec40ac41a56113a26eaff1489
David Foster <david@applied-data.science>
2020-12-07 13:22:24 +0000

    add comments

--- 2020/7/solution2.py
+++ 2020/7/solution2.py
@@ -1,6 +1,14 @@
-#script
+#imports
 import re
 
+#funcs
+def n_inside(suitcase):
+    counts = []
+    for colour, count in bags[suitcase].items():
+        counts.append(count * (1 + n_inside(colour)))
+    return sum(counts)
+
+#script
 data = open("input.txt", "r").read().splitlines()
 bags = {}
 total = 0
@@ -9,18 +17,14 @@ for row in data:
     bag = re.search(r'(.+) bags', bag).group(1)
 
     contains = contains_str.split(', ')
-
+    
+    #create 'bags' as a dict of dicts e.g. bags = {'shiny gold': {'dull pink': 3, ... }, ...}
     if ' no other bags.' in contains:
         bags[bag] = {}
     else:
-        count_bags = [int(re.search(r'(\d*) ?(.+) bags?', x.strip()).group(1)) for x in contains]
-        hold_bags = [re.search(r'(\d*) ?(.+) bags?', x.strip()).group(2) for x in contains]
+        regex = re.search(r'(\d*) ?(.+) bags?', x.strip())
+        count_bags = [int(regex.group(1)) for x in contains]
+        hold_bags = [regex.group(2) for x in contains]
         bags[bag] = dict(zip(hold_bags, count_bags))
-
-def n_inside(suitcase):
-    counts = []
-    for colour, count in bags[suitcase].items():
-        counts.append(count * (1 + n_inside(colour)))
-    return sum(counts)
 
 print(n_inside('shiny gold'))
