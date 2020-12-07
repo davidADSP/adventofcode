a6aca70e3c7473faa888d1fc8fc30d7dc623e7b1
David Foster <david@applied-data.science>
2020-12-07 17:56:53 +0000

    improve part 1

--- 2020/7/solution1.py
+++ 2020/7/solution1.py
@@ -5,15 +5,15 @@ data = open("input.txt", "r").read().splitlines()
 bags = {}
 total = 0
 for row in data:
-    bag, holds = row.split(' contain')
-    bag = re.search(r'(.+) bags', bag).group(1)
-    hold_bags = [re.search(r'(\d*) ?(.+) bags?', x.strip()).group(2) for x in holds.split(', ')]
-    bags[bag] = hold_bags
+    suitcase, contents = row.split(' contain')
+    suitcase = re.search(r'(.+) bags', suitcase).group(1)
+    contents = [re.search(r'(\d*) ?(.+) bags?', x.strip()).group(2) for x in contents.split(', ')]
+    bags[suitcase] = contents
 
 gold_bags = ['shiny gold']
 for colour in gold_bags:
-    for bag, hold_bags in bags.items():
-        if colour in hold_bags:
-            gold_bags.append(bag) if bag not in gold_bags else None
+    for suitcase, contents in bags.items():
+        if colour in contents:
+            gold_bags.append(suitcase) if suitcase not in gold_bags else None
 
-print(len(gold_bags) - 1)
+print(len(gold_bags) - 1) # don't count the shiny gold bag itself
