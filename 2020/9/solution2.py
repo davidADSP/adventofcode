3ca60a27a0f68a077a9d333269da7718d8796f87
David Foster <david@applied-data.science>
2020-12-09 09:17:03 +0000

    tweak

--- 2020/9/solution2.py
+++ 2020/9/solution2.py
@@ -3,21 +3,17 @@ data = [int(x) for x in open("input.txt", "r").read().splitlines()]
 INVALID = 69316178
 start = 0
 end = 1
-current_sum = 0
 cont = True
+
 while cont:
     rng = data[start:(end+1)]
-    current_sum = sum(rng)
-    while current_sum < INVALID:
+    while sum(rng) < INVALID:
         end+=1 
         rng = data[start:(end+1)]
-        current_sum = sum(rng)
-        
-        if current_sum == INVALID:
+        if sum(rng) == INVALID:
             print(min(rng) + max(rng))
             cont = False
             break
-
     start += 1
     end = start + 1
     
