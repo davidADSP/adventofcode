99941fa732873e65cdea71b516c47a2ca5d95c1c
davidADSP <david@adsp.ai>
2021-12-18 00:01:27 +0000

    day 17

--- 2021/17/solution1.py
+++ 2021/17/solution1.py
@@ -1,4 +1,4 @@
-xrng, yrng = [x for x in open("input.txt", "r").read().splitlines()][0][16:].split(', y=')
+xrng, yrng = [x for x in open("input.txt", "r").read().splitlines()][0][15:].split(', y=')
 
 xL, xR = [int(x) for x in xrng.split('..')]
 yB, yt = [int(x) for x in yrng.split('..')]
